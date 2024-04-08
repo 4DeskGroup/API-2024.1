39d17356632bec835bb5b6513ef9be1af99191f1
Stefanie Heinrich <102293897+ste-fa-nie@users.noreply.github.com>
2024-04-08 08:19:45 -0300

    home (adm-parceiro) - front

---
+++ api-componentes/Home [PAR]/components/Main/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Form} from 'react-native';
+import Forms from '../Form';
+export default function Main() {
+  return (
+    <View>
+      <Forms/>
+    </View>
+  );
+}
